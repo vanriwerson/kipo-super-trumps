df90cdd88c8097d1159a9fa3f86a41ad5762e045
Bruno Riwerson Silva <riwerson@gmail.com>
2025-10-03 11:48:10 -0300

    refactor: retorna somente as cartas em jogo no turno atual

--- src/services/turnServices/getCurrentTurnCards.ts
+++ src/services/turnServices/getCurrentTurnCards.ts
@@ -1,10 +1,7 @@
-import type { GameState } from '../../interfaces';
+import type { Card, GameState } from '../../interfaces';
 
-export const getCurrentTurnCards = (gameState: GameState): GameState => {
+export const getCurrentTurnCards = (gameState: GameState): Card[] => {
   const currentTurnCards = gameState.players.map((player) => player.hand[0]);
 
-  return {
-    ...gameState,
-    currentTurnCards,
-  };
+  return currentTurnCards;
 };
