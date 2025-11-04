12133e4b4fd0bfa5317f3a2004882b02a7a16e52
Bruno Riwerson Silva <riwerson@gmail.com>
2025-11-04 09:09:52 -0300

    feat: cria página home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import { GameRules } from '../../components';
 import './style.css';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="page-container">
+      <div className="presentation">
+        <p>
+          Prepare-se para uma verdadeira viagem no tempo! O lendário{' '}
+          <strong>Super Trunfo</strong> está de volta com um visual renovado e
+          personagens incríveis. Reviva a emoção das cartas, enfrente a IA e
+          reviva essa experiência dos anos 80 — o trunfo está em suas mãos!
+        </p>
+      </div>
+
+      <GameRules />
+
+      <NavLink to="/game" className="nav">
+        Bora Jogar!
+      </NavLink>
+    </div>
+  );
 }
