06b77ebc2b1ecc235880b157ea8cd069d7159e6f
Bruno Riwerson Silva <riwerson@gmail.com>
2025-11-04 09:06:10 -0300

    fix: ajusta import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import routes from './routes/index.tsx';
+import routes from './routes';
 import './index.css';
 
 const router = createBrowserRouter(routes, {
